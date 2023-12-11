990b2f68409f04ce05999959d97938c4ce65a1d0
Zain Ali <zain.ali@hva.nl>
2023-12-11 11:18:35 +0100

    use listenfd

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,6 @@
 use anyhow::Result;
+use listenfd::ListenFd;
+use tokio::net::TcpListener;
 
 mod db;
 mod router;
@@ -7,17 +9,17 @@ mod router;
 async fn main() -> Result<()> {
   dotenvy::dotenv()?;
 
-  let db = db::init_db::init_db().await?;
+  let mut listenfd = ListenFd::from_env();
 
-  let app = router::get_router()
-    .with_state(db);
-
-  let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-    .await
-    .unwrap();
+  let listener = match listenfd.take_tcp_listener(0).unwrap() {
+    Some(listener) => TcpListener::from_std(listener).unwrap(),
+    None => TcpListener::bind("127.0.0.1:3000").await.unwrap(),
+  };
 
   println!("listening on {}", listener.local_addr().unwrap());
 
+  let db = db::init_db::init_db().await?;
+  let app = router::get_router().with_state(db);
   axum::serve(listener, app).await.unwrap();
 
   Ok(())
