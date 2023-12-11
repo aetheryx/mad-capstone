68ce356e60d40fa3318aa9fa1067898c7ade6bce
aetheryx <mhzy.ali@gmail.com>
2023-12-11 11:34:30 +0100

    add timeout svc

--- backend/src/router/mod.rs
+++ backend/src/router/mod.rs
@@ -1,12 +1,30 @@
-use axum::Router;
+use std::time::Duration;
+
 use sea_orm::DatabaseConnection;
+
+use axum::{error_handling::HandleErrorLayer, http::StatusCode, Router};
+use tower::{BoxError, ServiceBuilder};
 
 use self::auth::auth_router;
 
-mod auth;
 mod app_error;
+mod auth;
 
 pub fn get_router() -> Router<DatabaseConnection> {
-  Router::new().nest("/auth", auth_router())
-}
+  let timeout_svc = ServiceBuilder::new()
+    .layer(HandleErrorLayer::new(|error: BoxError| async move {
+      if error.is::<tower::timeout::error::Elapsed>() {
+        Ok(StatusCode::REQUEST_TIMEOUT)
+      } else {
+        Err((
+          StatusCode::INTERNAL_SERVER_ERROR,
+          format!("Unhandled internal error: {error}"),
+        ))
+      }
+    }))
+    .timeout(Duration::from_secs(10));
 
+  Router::new()
+    .nest("/auth", auth_router())
+    .layer(timeout_svc.into_inner())
+}
