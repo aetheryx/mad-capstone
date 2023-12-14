51abec2e0b472a021c0f9cc6244a256adc8ca500
aetheryx <mhzy.ali@gmail.com>
2023-12-14 10:39:45 +0100

    add dependencies

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -51,19 +51,19 @@ android {
 }
 
 dependencies {
-  implementation("androidx.core:core-ktx:1.9.0")
+  implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-  implementation("androidx.activity:activity-compose:1.8.0")
-  implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+  implementation("androidx.activity:activity-compose:1.8.1")
+  implementation(platform("androidx.compose:compose-bom:2023.10.01"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("androidx.compose.material3:material3")
-  testImplementation("junit:junit:4.13.2")
-  androidTestImplementation("androidx.test.ext:junit:1.1.5")
-  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-  androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-  androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+//  testImplementation("junit:junit:4.13.2")
+//  androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//  androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+//  androidTestImplementation("androidx.compose.ui:ui-test-junit4")
   debugImplementation("androidx.compose.ui:ui-tooling")
   debugImplementation("androidx.compose.ui:ui-test-manifest")
 
@@ -71,22 +71,30 @@ dependencies {
   implementation("androidx.compose.material:material-icons-extended")
 
   // live data
-  implementation("androidx.compose.runtime:runtime-livedata:1.5.1")
+  implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
 
   // navigation
-  implementation("androidx.navigation:navigation-compose:2.7.3")
-  implementation("androidx.compose.material:material:1.5.1")
+  implementation("androidx.navigation:navigation-compose:2.7.5")
+  implementation("androidx.compose.material:material:1.5.4")
 
   // splash screen
-  implementation("androidx.core:core-splashscreen:1.0.0")
+  implementation("androidx.core:core-splashscreen:1.0.1")
 
   // network libraries
-  implementation("com.squareup.retrofit2:retrofit:2.8.1")
-  implementation("com.squareup.okhttp3:okhttp:4.9.3")
-  implementation("com.squareup.okhttp3:logging-interceptor:4.4.0")
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.squareup.okhttp3:okhttp:4.12.0")
+  implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
   implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
 
   // datastore
   implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+  // firebase
+  implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+  implementation("com.google.firebase:firebase-storage")
+
+  // image loading
+  implementation("io.coil-kt:coil:2.1.0")
+  implementation("io.coil-kt:coil-compose:2.1.0")
 }
