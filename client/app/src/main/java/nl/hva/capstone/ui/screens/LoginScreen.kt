cd1cd1d8bc8cb016a134ab5717c00d4561f7cede
Zain Ali <zain.ali@hva.nl>
2023-12-14 10:39:23 +0100

    refactor login screen, extract text field section

--- client/app/src/main/java/nl/hva/capstone/ui/screens/LoginScreen.kt
+++ client/app/src/main/java/nl/hva/capstone/ui/screens/LoginScreen.kt
@@ -2,6 +2,7 @@ package nl.hva.capstone.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,6 +15,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -33,17 +35,15 @@ import nl.hva.capstone.R
 import nl.hva.capstone.viewmodels.SessionState
 import nl.hva.capstone.viewmodels.SessionViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(
   navController: NavHostController,
   sessionViewModel: SessionViewModel
 ) {
-  var username by remember { mutableStateOf("") }
-  var password by remember { mutableStateOf("") }
-
   val state by sessionViewModel.state.observeAsState()
-  val isError = state == SessionState.CREDENTIAL_ERROR
+  val error =
+    if (state == SessionState.CREDENTIAL_ERROR) stringResource(R.string.password_is_incorrect)
+    else null
 
   if (state == SessionState.READY) {
     navController.navigate("/home")
@@ -53,64 +53,97 @@ fun LoginScreen(
     modifier = Modifier
       .fillMaxSize()
       .padding(16.dp),
-    verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
+    verticalArrangement = Arrangement.SpaceBetween
   ) {
-    TextField(
-      value = username,
-      onValueChange = { username = it },
-      label = { Text(stringResource(R.string.username)) },
-      modifier = Modifier.fillMaxWidth(),
-      colors = TextFieldDefaults.textFieldColors(),
-      shape = TextFieldDefaults.filledShape,
-    )
+    Column {}
 
-    TextField(
-      value = password,
-      onValueChange = { password = it },
-      label = { Text(stringResource(R.string.password)) },
-      modifier = Modifier.fillMaxWidth(),
-      colors = TextFieldDefaults.textFieldColors(),
-      shape = TextFieldDefaults.filledShape,
-      isError = isError,
-      visualTransformation = PasswordVisualTransformation(),
-      supportingText = {
-        if (isError) {
-          Text(
-            stringResource(R.string.password_is_incorrect),
-            color = MaterialTheme.colorScheme.error,
-          )
-        }
-      },
-      trailingIcon = {
-        if (isError) {
-          Icon(
-            Icons.Filled.Error,
-            contentDescription = "Error",
-            tint = MaterialTheme.colorScheme.error
-          )
-        }
-      }
-    )
-
-    Button(
-      onClick = {
-        sessionViewModel.logIn(username, password)
-      },
-      modifier = Modifier.fillMaxWidth(),
-      colors = ButtonDefaults.buttonColors()
+    Column(
+      verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
     ) {
-      when (state) {
-        SessionState.LOGGING_IN -> {
-          CircularProgressIndicator(
-            color = MaterialTheme.colorScheme.onPrimary,
-            strokeWidth = 3.dp,
-            modifier = Modifier.size(24.dp),
-          )
-        }
-        else -> {
-          Text(stringResource(R.string.log_in))
+      LogInSection(
+        error,
+        onSubmit = sessionViewModel::logIn
+      ) {
+        when (state) {
+          SessionState.LOGGING_IN -> {
+            CircularProgressIndicator(
+              color = MaterialTheme.colorScheme.onPrimary,
+              strokeWidth = 3.dp,
+              modifier = Modifier.size(24.dp),
+            )
+          }
+          else -> {
+            Text(stringResource(R.string.log_in))
+          }
         }
       }
     }
+
+    Column {
+      OutlinedButton(
+        onClick = {
+          navController.navigate("/signup")
+        },
+        colors = ButtonDefaults.outlinedButtonColors(),
+        modifier = Modifier.fillMaxWidth()
+      ) {
+        Text(stringResource(R.string.create_new_account))
+      }
+    }
   }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LogInSection(
+  error: String?,
+  onSubmit: (String, String) -> Unit,
+  buttonContent: @Composable RowScope.() -> Unit,
+) {
+  var username by remember { mutableStateOf("") }
+  var password by remember { mutableStateOf("") }
+
+  TextField(
+    value = username,
+    onValueChange = { username = it },
+    label = { Text(stringResource(R.string.username)) },
+    modifier = Modifier.fillMaxWidth(),
+    colors = TextFieldDefaults.textFieldColors(),
+    shape = TextFieldDefaults.filledShape,
+  )
+
+  TextField(
+    value = password,
+    onValueChange = { password = it },
+    label = { Text(stringResource(R.string.password)) },
+    modifier = Modifier.fillMaxWidth(),
+    colors = TextFieldDefaults.textFieldColors(),
+    shape = TextFieldDefaults.filledShape,
+    isError = error != null,
+    visualTransformation = PasswordVisualTransformation(),
+    supportingText = {
+      if (error != null) {
+        Text(
+          error,
+          color = MaterialTheme.colorScheme.error,
+        )
+      }
+    },
+    trailingIcon = {
+      if (error != null) {
+        Icon(
+          Icons.Filled.Error,
+          contentDescription = "Error",
+          tint = MaterialTheme.colorScheme.error
+        )
+      }
+    }
+  )
+
+  Button(
+    onClick = { onSubmit(username, password) },
+    modifier = Modifier.fillMaxWidth(),
+    colors = ButtonDefaults.buttonColors(),
+    content = buttonContent
+  )
 }
