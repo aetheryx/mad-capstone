7b7d3310fd158e60c0befd8114858ed085bde844
Zain Ali <zain.ali@hva.nl>
2023-12-28 15:27:58 +0100

    call notifications

--- client/app/src/main/java/nl/hva/capstone/service/NotificationEventHandler.kt
+++ client/app/src/main/java/nl/hva/capstone/service/NotificationEventHandler.kt
@@ -1,16 +1,26 @@
 package nl.hva.capstone.service
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Person
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import nl.hva.capstone.CapstoneApplication
 import nl.hva.capstone.R
+import nl.hva.capstone.activities.CallActivity
 import nl.hva.capstone.api.ServerEvent
+import nl.hva.capstone.api.model.input.IncomingCallOffer
 import nl.hva.capstone.api.model.output.ConversationMessage
+import nl.hva.capstone.api.model.output.OutgoingCallOffer
+import java.time.Instant
+import java.util.Date
 
 enum class NotificationChannelID {
   Message,
@@ -34,8 +44,10 @@ class NotificationEventHandler(
   }
 
   private fun onServerEvent(event: ServerEvent) {
+    Log.v("NotificationEventHandler", "$event")
     when (event) {
       is ServerEvent.MessageCreateEvent -> handleMessageCreate(event.data)
+      is ServerEvent.CallOfferEvent -> handleCallOfferEvent(event.data)
       else -> {}
     }
   }
@@ -53,13 +65,40 @@ class NotificationEventHandler(
       .find { it.id == message.conversationID }!!
       .otherParticipant.username
 
-    val notification = NotificationCompat.Builder(application, channelID)
+    val notification = Notification.Builder(application, channelID)
         .setContentTitle(title)
         .setContentText(message.content)
         .setSmallIcon(R.drawable.default_pfp)
-        .setPriority(NotificationCompat.PRIORITY_HIGH)
         .build()
 
     manager.notify(message.id, notification)
+  }
+
+  private fun handleCallOfferEvent(callOffer: OutgoingCallOffer) {
+    val manager = application.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    val channelID = NotificationChannelID.Call.toString()
+    val channel = NotificationChannel(channelID, channelID, NotificationManager.IMPORTANCE_HIGH)
+    manager.createNotificationChannel(channel)
+
+    val person = Person.Builder()
+      .setName("bob")
+      .build()
+
+    val callIntent = Intent(application, CallActivity::class.java)
+
+    val answerIntent = PendingIntent.getActivity(application.applicationContext, 0, callIntent,
+      PendingIntent.FLAG_IMMUTABLE)
+
+    val declineIntent = PendingIntent.getActivity(application.applicationContext, 0, callIntent,
+      PendingIntent.FLAG_IMMUTABLE)
+
+    val notification = Notification.Builder(application, channelID)
+      .setSmallIcon(R.drawable.default_pfp)
+      .setStyle(Notification.CallStyle.forIncomingCall(person, answerIntent, declineIntent))
+      .setFullScreenIntent(answerIntent, true)
+      .build()
+
+    manager.notify(Instant.now().epochSecond.toInt(), notification)
   }
 }
