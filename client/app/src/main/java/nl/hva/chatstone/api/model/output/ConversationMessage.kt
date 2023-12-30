ebeae58ed381b729b664ae9667f2fad2fff506ff
Zain Ali <zain.ali@hva.nl>
2023-12-30 18:38:59 +0100

    message.createdAt getter

--- client/app/src/main/java/nl/hva/chatstone/api/model/output/ConversationMessage.kt
+++ client/app/src/main/java/nl/hva/chatstone/api/model/output/ConversationMessage.kt
@@ -3,6 +3,8 @@ package nl.hva.chatstone.api.model.output
 import androidx.compose.runtime.Immutable
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @Serializable
 @Immutable
@@ -11,5 +13,8 @@ data class ConversationMessage(
   @SerialName("author_id") val authorID: Int,
   @SerialName("conversation_id") val conversationID: Int,
   val content: String,
-  @SerialName("created_at") val createdAt: String
-)
+  @SerialName("created_at") val createdAtRaw: String
+) {
+  val createdAt: ZonedDateTime
+    get() = ZonedDateTime.parse("${createdAtRaw}Z", DateTimeFormatter.ISO_DATE_TIME)
+}
