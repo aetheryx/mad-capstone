fc11ec7ba560daffa3756862bb641531526316ad
Zain Ali <zain.ali@hva.nl>
2024-01-02 19:44:39 +0100

    fix signup

--- client/app/src/main/java/nl/hva/chatstone/api/model/input/SignupInput.kt
+++ client/app/src/main/java/nl/hva/chatstone/api/model/input/SignupInput.kt
@@ -1,11 +1,10 @@
 package nl.hva.chatstone.api.model.input
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class SignupInput(
   val username: String,
   val password: String,
-  @SerialName("profile_picture_url") val profilePictureURL: String
+  val avatar: String
 )
