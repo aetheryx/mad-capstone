5892f62325d187cb9a5e2b634b2bfb530f0e7926
aetheryx <mhzy.ali@gmail.com>
2024-01-08 14:38:16 +0100

    agp upgrade

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -71,19 +71,23 @@ android {
 dependencies {
   implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-  implementation("androidx.activity:activity-compose:1.8.1")
+  implementation("androidx.activity:activity-compose:1.8.2")
   implementation(platform("androidx.compose:compose-bom:2023.10.01"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("androidx.compose.material3:material3")
-//  testImplementation("junit:junit:4.13.2")
-//  androidTestImplementation("androidx.test.ext:junit:1.1.5")
-//  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-//  androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-//  androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+  // debugger
   debugImplementation("androidx.compose.ui:ui-tooling")
   debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+  // tests
+  testImplementation("junit:junit:4.13.2")
+  androidTestImplementation("androidx.test.ext:junit:1.1.5")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+  androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+  androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
   // material icons
   implementation("androidx.compose.material:material-icons-extended")
@@ -99,7 +103,7 @@ dependencies {
   // splash screen
   implementation("androidx.core:core-splashscreen:1.0.1")
 
-  // network libraries
+  // networking
   implementation("com.squareup.retrofit2:retrofit:2.9.0")
   implementation("com.squareup.okhttp3:okhttp:4.12.0")
   implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
@@ -117,9 +121,6 @@ dependencies {
   implementation("io.coil-kt:coil:2.1.0")
   implementation("io.coil-kt:coil-compose:2.1.0")
 
-  // notifications
-  implementation("com.google.accompanist:accompanist-permissions:0.30.1")
-
   // webrtc
-  implementation("io.getstream:stream-webrtc-android:1.1.0")
+  implementation("io.getstream:stream-webrtc-android:1.1.1")
 }
